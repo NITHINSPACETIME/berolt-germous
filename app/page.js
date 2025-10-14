e52f4f07942e1cd5c05a497d839e5519d86e8f1c
Your Name <you@example.com>
2025-10-14 20:55:28 +0530

    2 hours of learning

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-import Image from "next/image";
+import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
+  const isLoggedIn = true;
+  const name = "Nithin";
   return (
     <main>
       <h1> Collect customer feedback to build better products </h1>
-      <div> 
-        the subsection of the customer feedback section
-      </div>
+      <div>the subsection of the customer feedback section</div>
+      <ButtonLogin isLoggedIn={isLoggedIn}  name={name}/>
     </main>
   );
 }

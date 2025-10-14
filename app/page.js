88756ba2e0142523190b8c375e77855e4f6d1b47
Your Name <you@example.com>
2025-10-14 13:38:06 +0530

    updated the superpage

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1> Collect customer feedback to build better products </h1>
+      <div> 
+        the subsection of the customer feedback section
+      </div>
     </main>
   );
 }
